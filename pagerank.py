6e0aed39f41c1b2350deadaa2b435610761ced41
Hannah He <hannahhe18@gmail.com>
2018-12-13 17:37:54 -0500

    irreducible matrices functional, and dangling nodes functional.

--- pagerank.py
+++ pagerank.py
@@ -1,6 +1,8 @@
 import numpy as np
+import math
 chain = [[1,2,3],[3],[1,3],[1,2]]
-
+chain1 = [[1,2,3],[3],[0,3],[0,2]]
+taischain = [[1,2,3],[3],[0,3],[]]
 def probMatrix(chain):
     #Probability Matrix
     #M_{ij) = 1 / len
@@ -11,42 +13,53 @@ def probMatrix(chain):
             isDG = True
         for i in range(len(chain)):
             if isDG:
-                M[i][j] = 1/len(chain)
+                M[j][i] = 1/len(chain)
             elif j in chain[i]:
-                M[i][j] = 1 / len(chain[i]) 
+                M[j][i] = 1 / len(chain[i])
         isDG = False
     return M
 
 def isDanglingNode(chain, j):
     return chain[j] == []
-          
+
 def isReducible(chain):
     pass
+
 
 def rank(chain):
     x = [1 for i in range(len(chain))]
     M = probMatrix(chain)
     arrEig = np.linalg.eig(M)
-    eigVal = arrEig[0].tolist() #list of eigenvalues
-    eigVec = arrEig[1] #matrix of eigenvectors
-    
-    #fix rounding
-    '''for i in range(len(eigVec)):
-        for j in range(len(eigVec[i])):
-            eigVec[i][j] = round(eigVec[i][j], 2)'''
-    
+    eigVal = list(arrEig[0]) #list of eigenvalues
+    eigVec = arrEig[1] #matrix of eigenvectors = P
+    min_index = -1
+    min_dist = float('inf')
     for i in range(len(eigVal)):
-        eigVal[i] = round(eigVal[i], 1)
-    
-    i = eigVal.index(1) #index of eigenval 1
-    evOne = eigVec[i] #eigvector for eigenval 1
+        lambda_i = eigVal[i]
+        dist = abs(lambda_i -1)
+        if(dist < min_dist):
+            min_index = i
+            min_dist = dist
+    evOne = eigVec[:,min_index]
+    for i in range(len(evOne)):
+        if(evOne[i] < 0):
+            evOne[i] *= -1
     eigVecInv = np.linalg.inv(eigVec) #inverse of eigenvector matrix
-    
+    n = len(chain)
+    C = np.zeros((n,n))
+    C[:,0] = evOne
+    print(evOne)
     #Compute [eigenvector of 1] * P^-1 * x_0
-    print(eigVec.tolist())
-    print(evOne, eigVecInv)
-    return np.multiply(np.multiply(evOne, eigVecInv), x)
-    
-    
-print(rank(chain))
-    
+    prod = np.matmul(C, eigVecInv)
+    prod1 = np.matmul(prod,x)
+    result = [0 for i in range(n)]
+    for i in range(len(prod1)):
+        result[i] = np.abs(prod1[i])
+    print("RES", result)
+    result = [j for (i,j) in sorted([(s,t) for (t,s) in enumerate(result)])]
+    result = result[::-1]
+    return result
+
+
+
+print(rank(taischain))
